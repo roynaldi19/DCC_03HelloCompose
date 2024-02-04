02cb0fe510e6559399c157f775dd5539990b3839
roynaldi19 <roynaldi19@gmail.com>
2024-02-04 21:52:21 +0700

    menyusun item

--- app/src/main/java/com/roynaldi19/dcc_03hellocompose/MainActivity.kt
+++ app/src/main/java/com/roynaldi19/dcc_03hellocompose/MainActivity.kt
@@ -3,13 +3,26 @@ package com.roynaldi19.dcc_03hellocompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.roynaldi19.dcc_03hellocompose.ui.theme.DCC_03HelloComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -30,12 +43,29 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Greeting(name: String) {
+    Row(
+        modifier = Modifier.padding(8.dp),
+        verticalAlignment  = Alignment.CenterVertically
+    ) {
+
+        Image(
+            painter = painterResource(R.drawable.jetpack_compose),
+            contentDescription = "Logo Jetpack Compose",
+            modifier = Modifier.size(80.dp)
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+        Column {
+            Text(
+                text = "Hello $name!",
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold
+            )
+            Text(text = "Welcome to Dicoding!")
+        }
+    }
 }
+
 
 @Preview(showBackground = true)
 @Composable
